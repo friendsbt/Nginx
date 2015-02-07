ec6fdc023f1583a6ad240c7f6079740df3efccc7
laike9m <laike9m@gmail.com>
2015-02-07 17:55:25 +0800

    change cookie name

--- src/test.py
+++ src/test.py
@@ -2,7 +2,7 @@ import requests
 
 r0 = requests.get("http://127.0.0.1:8081/test")
 print("r0 " + str(r0.status_code) + '\n' + r0.text)
-r1 = requests.get("http://127.0.0.1:8081/test", cookies={"fbt_uid": "test_uid"})
+r1 = requests.get("http://127.0.0.1:8081/test", cookies={"fbt_user_id": "test_uid"})
 print("r1 " + str(r1.status_code) + '\n' + r1.text)
-r2 = requests.get("http://127.0.0.1:8081/test?token=test_token", cookies={"fbt_uid": "test_uid"})
+r2 = requests.get("http://127.0.0.1:8081/test?token=test_token", cookies={"fbt_user_id": "test_uid"})
 print("r2 " + str(r2.status_code) + '\n' + r2.text)
