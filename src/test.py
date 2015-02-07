e2e614b1701f987615319fb75d0b88e2b3ac62d2
laike9m <laike9m@gmail.com>
2015-02-07 16:50:58 +0800

    use 8081, avoid redis_key not set error

--- src/test.py
+++ src/test.py
@@ -1,8 +1,8 @@
 import requests
 
-r0 = requests.get("http://127.0.0.1:8080/test")
+r0 = requests.get("http://127.0.0.1:8081/test")
 print("r0 " + str(r0.status_code) + '\n' + r0.text)
-r1 = requests.get("http://127.0.0.1:8080/test", cookies={"fbt_uid": "test_uid"})
+r1 = requests.get("http://127.0.0.1:8081/test", cookies={"fbt_uid": "test_uid"})
 print("r1 " + str(r1.status_code) + '\n' + r1.text)
-r2 = requests.get("http://127.0.0.1:8080/test?token=test_token", cookies={"fbt_uid": "test_uid"})
+r2 = requests.get("http://127.0.0.1:8081/test?token=test_token", cookies={"fbt_uid": "test_uid"})
 print("r2 " + str(r2.status_code) + '\n' + r2.text)
