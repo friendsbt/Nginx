cefd0b1bce999ca54d3bffc08f01d20f28a1e5e8
laike9m <laike9m@gmail.com>
2015-02-07 10:29:08 +0800

    add sbin path when configure nginx

--- src/configure-nginx.sh
+++ src/configure-nginx.sh
@@ -1,1 +1,1 @@
-./configure --add-module='../nginx-eval-module-2014.05.09' --add-module='../ngx_http_redis-0.3.7' --add-module='../echo-nginx-module-0.57' --with-cc-opt="-Wno-deprecated-declarations"
+./configure --sbin-path=/usr/local/sbin --add-module='../nginx-eval-module-2014.05.09' --add-module='../ngx_http_redis-0.3.7' --add-module='../echo-nginx-module-0.57' --with-cc-opt="-Wno-deprecated-declarations"
